659fc83d907d941cbb2e586a53cac98cf4d021ed
Michele Renda <michele.renda@cern.ch>
2025-10-19 13:13:24 +0300

    Added check for QT binding

--- src/magboltz_gui/main.py
+++ src/magboltz_gui/main.py
@@ -1,16 +1,24 @@
 from importlib.resources import files
 
-from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QApplication
-from PyQt6.QtCore import Qt
 import sys
 
 from magboltz_gui.util.platform import ensure_qt_runtime_or_explain
-from magboltz_gui.window.main_window import MagboltzGUI
+
 
 
 def main() -> None:
+
+    ensure_qt_runtime_or_explain()
+
+    from PyQt6.QtGui import QIcon
+    from PyQt6.QtWidgets import QApplication
+    from PyQt6.QtCore import Qt
+    from magboltz_gui.window.main_window import MagboltzGUI
+
     app: QApplication = QApplication(sys.argv)
+
+
+
     app.setWindowIcon(QIcon(str(files("magboltz_gui.icons").joinpath("icon-192x192.png"))))
     window: MagboltzGUI = MagboltzGUI()
     window.setWindowFlag(Qt.WindowType.Window)
